0a48104a696f34a340de95d859a3cdb45a7a5466
pvijaykrishna549 <you@example.com>
2021-05-24 09:14:10 +0530

    Added HotelList with differnt Rates

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -1,8 +1,44 @@
 package com.bridgelabz.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class AddHotel{
+    String HotelName;
+    int HotelRating;
+    int WeekRate_for_Regular_Customers;
+    int Week_Rate_for_Reward_Customers;
+    int Weekend_Rate_for_Regular_Customers;
+    int Weekend_Rate_for_Reward_customers;
+
+    public AddHotel(String HotelName,int HotelRating ,int WeekRate_for_Regular_Customers,
+                    int Week_Rate_for_Reward_Customers,int Weekend_Rate_for_Regular_Customers,
+                    int Weekend_Rate_for_Reward_customers){
+        this.HotelName = HotelName;
+        this.HotelRating = HotelRating;
+        this.Week_Rate_for_Reward_Customers = Week_Rate_for_Reward_Customers;
+        this.WeekRate_for_Regular_Customers = WeekRate_for_Regular_Customers;
+        this.Weekend_Rate_for_Regular_Customers = Weekend_Rate_for_Regular_Customers;
+        this.Weekend_Rate_for_Reward_customers = Weekend_Rate_for_Reward_customers;
+    }
+
+}
+
 public class HotelReservationSystem {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System");
+
+        List<AddHotel> holtelList = new ArrayList<>();
+        holtelList.add(new AddHotel("Lakewood",3,110,
+                80,90,80));
+        holtelList.add(new AddHotel("BridgeWood", 4, 160,
+                110,
+                60,50));
+        holtelList.add(new AddHotel("RidgeWood",5,220,
+                100,150,
+                150));
+
+
     }
 }
